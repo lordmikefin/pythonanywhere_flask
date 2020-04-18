54346baaa9a778b3270922e9e9a02d0373d23e96
LordMike <lordmike@iki.fi>
2020-04-18 18:43:49 +0300

    do not forgot the routes :)

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ def home():
     # etc etc, flask app code
 	return 'Test string :)'
 	
+@app.route('/redirect_test')
 def redirect_test():
 	return redirect('https://fi.linkedin.com/in/lordmikefin')
 
+@app.route('/linkedn')
 def linkedn():
 	return '<script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script><div class="LI-profile-badge"  data-version="v1" data-size="large" data-locale="en_US" data-type="horizontal" data-theme="light" data-vanity="lordmikefin"><a class="LI-simple-link" href="https://fi.linkedin.com/in/lordmikefin?trk=profile-badge">Mikko Niemelä</a></div>'
 
