03ed4c08c167370d0be97f4cf7e0e7592806c772
LordMike <lordmike@iki.fi>
2020-04-18 18:42:34 +0300

    testing linkedin badge

--- app.py
+++ app.py
@@ -5,8 +5,13 @@ app = Flask("TEST")
 @app.route('/')
 def home():
     # etc etc, flask app code
-	#return 'Test string :)'
-	return redirect('https://www.linkedin.com/in/lordmikefin')
+	return 'Test string :)'
+	
+def redirect_test():
+	return redirect('https://fi.linkedin.com/in/lordmikefin')
+
+def linkedn():
+	return '<script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script><div class="LI-profile-badge"  data-version="v1" data-size="large" data-locale="en_US" data-type="horizontal" data-theme="light" data-vanity="lordmikefin"><a class="LI-simple-link" href="https://fi.linkedin.com/in/lordmikefin?trk=profile-badge">Mikko Niemelä</a></div>'
 
 if __name__ == '__main__':
     app.run()
