e31f9fe9fbcbe17e4dafd189de5d10ff21107949
LordMike <lordmike@iki.fi>
2020-04-18 18:21:00 +0300

    just rediret to my linkedin page :)

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask("TEST")
 @app.route('/')
 def home():
     # etc etc, flask app code
-	return 'Test string :)'
+	#return 'Test string :)'
+	return redirect('https://www.linkedin.com/in/lordmikefin')
 
 if __name__ == '__main__':
     app.run()
